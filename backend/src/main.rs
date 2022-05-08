90c1215c1cc11006ad5ef9a9b3cd3960a520f7ff
David Turner <git@dwt27.co.uk>
2022-05-08 16:52:53 +0000

    Add battery details to backend packet parser

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -2,12 +2,15 @@ use warp::Filter;
 use serde_derive::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize, Serialize)]
-struct Fix {
+struct Packet {
     long: f64,
     lat: f64,
     alt: f32,
     time: String,
     sats: usize,
+    voltage: f32,
+    current: f32,
+    soc: f32,
 }
 
 #[tokio::main]
@@ -17,9 +20,9 @@ async fn main() {
         // Only accept bodies smaller than 16kb
         .and(warp::body::content_length_limit(1024 * 16))
         .and(warp::body::json())
-        .map(|fix: Fix| {
-            println!("Rx: {:#?}", fix);
-            println!("https://maps.google.com/?q={},{}", fix.lat, fix.long);
+        .map(|packet: Packet| {
+            println!("Rx: {:#?}", packet);
+            println!("https://maps.google.com/?q={},{}", packet.lat, packet.long);
             println!("");
             warp::reply()
         });
