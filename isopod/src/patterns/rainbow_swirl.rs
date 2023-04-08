6ce2371d3706b4ac185b5f79bbefa22a564b3e6a
David Turner <git@dwt27.co.uk>
2023-04-08 18:58:14 +0100

    Remove redundant theta smear parameter from rainbow swirl

--- isopod/src/patterns/rainbow_swirl.rs
+++ isopod/src/patterns/rainbow_swirl.rs
@@ -36,7 +36,6 @@ impl Pattern for RainbowSwirl {
 
     fn step(&mut self, _gps: &Option<GpsFix>, _imu: &ImuReadings) -> &LedUpdate {
         let radial_smear: f64 = SETTINGS.get("rainbow_swirl_radial_smear").unwrap();
-        let rotary_smear: f64 = SETTINGS.get("rainbow_swirl_rotary_smear").unwrap();
         let speed: f64 = SETTINGS.get("rainbow_swirl_speed").unwrap();
 
         // Turn all LEDs off
@@ -45,7 +44,7 @@ impl Pattern for RainbowSwirl {
                 let rratio = r as f64 / LEDS_PER_SPINE as f64;
                 let r_comp = rratio * 360.0 * radial_smear;
                 let tratio = theta as f64 / SPINES as f64;
-                let a_comp = tratio * 360.0 * rotary_smear;
+                let a_comp = tratio * 360.0;
 
                 let hue = (r_comp + a_comp + self.t).rem_euclid(360.0);
 
